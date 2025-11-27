46b99c194e820e70565867b2866c00ef93fb8b0e
Mulugeta Negussie <negussim@gmail.com>
2025-11-27 18:25:15 -0500

    Update website files and add data cleaning notebook

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(
     __name__,
-    template_folder="website/templates",
-    static_folder="website/static"
+    template_folder="Website/templates",
+    static_folder="Website/static"
 )
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
